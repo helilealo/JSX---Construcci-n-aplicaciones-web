df992a4987ebc0ed5179c5529f2e02ed9709ced4
helilealo <samuel.lealo@uniagustiniana.edu.co>
2026-09-10 09:39:15 -0500

    Add files via upload

---
+++ src (1)/main (1).jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import Tarjeta from "./Orquestador/Tarjeta";
+import Obras from "./data.json";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <div className="pagina">
+
+      <header className="hero">
+        <h1>GalHelyColl</h1>
+        <p>Tienda de arte</p>
+      </header>
+
+      <main className="productos">
+
+        {Obras.map((obra) => (
+          <Tarjeta
+            key={obra.id}
+            title={obra.title}
+            descripcion={obra.descripcion}
+            color={obra.color}
+            img={obra.img}
+          />
+        ))}
+
+      </main>
+
+    </div>
+  </StrictMode>
+);
